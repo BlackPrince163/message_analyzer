79e3b185da42d7bdca1fcaa0d15e4cb0ab8f77f0
BlackPrince163 <4523120100Ni>
2022-04-13 23:49:22 +0300

    + models message & register this models to admin

--- message_analyzer/analyzer/models.py
+++ message_analyzer/analyzer/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Message(models.Model):
+    STATUS = [
+        ('1', 'review'),
+        ('2', 'blocked'),
+        ('3', 'correct'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
+    status = models.CharField(choices=STATUS, default='1', max_length=1)
+    datetime = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return "{}, content: {}. Status: {}".format(self.user, self.content, self.get_status_display())

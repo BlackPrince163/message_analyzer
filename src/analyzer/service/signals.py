c1221dbbc5875aaf9e3dde23f80574c344a791a5
BlackPrince163 <4523120100Ni>
2022-04-15 23:39:40 +0300

    - listener

--- src/analyzer/service/signals.py
+++ src/analyzer/service/signals.py
@@ -1,11 +1,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from . import listener
-
+from analyzer.task import test_task
 
 @receiver(post_save)
 def start_listener(sender, **kwargs):
     message = kwargs.get('instance')
     if kwargs.get('created'):
-        listener.start(message)
+        test_task.delay(message.id)
